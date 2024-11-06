1f5f30257bb64bb225c5610ad553087c61668a31
firqaaa <firqaaa@gmail.com>
2024-11-06 21:18:02 +0700

    update routing.py

--- routing.py
+++ routing.py
@@ -8,7 +8,7 @@ routing_prompt = """Given the user question below, classify the user's intention
 Do not respond with more than one word.
 
 Greeting: If the user greets you.
-Question: If the user asks a general question about the company.
+Question: If the user asks all about the BPJS company.
 Summary: If the user specifically asks you to create a summary of the chat.
 Farewell: If the user says goodbye or farewell.
 Location: If the user asks about hospital locations.
